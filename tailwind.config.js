deaa51e24c99bb48ec833e39e59480a5ae568f03
Sirori <tldus7519@gmail.com>
2023-07-28 23:25:36 +0900

    [add] 1일차 중간 커밋(icon 파일 추가, eslint 설정, tailwind 확장)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./client/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        sm: '435px',
+        md: '768px',
+        lg: '976px',
+        xl: '1050px',
+      },
+      colors: {
+        'primary': '#5f0080',
+        'content': '#333333',
+        'accent-yellow': '#fa622f',
+        'light-gray-bg': '#E1E1E1',
+        'light-gray-text': '#898989',
+        'light-purple': '#2A0038',
+        'gray-light': '#d3dce6',
+      },
+      fontSize: {
+        10: '10px',
+        21: '21px',
+        22: '22px',
+        28: '28px',
+        38: '38px'
+      },
+      fontFamily: {
+        'pretendard': ['"Pretendard Variable", Pretendard'],
+      }
+    },
   },
   plugins: [],
 };
