1ceec3483595410a93c55e04649f5b4e314b5b07
Sirori <tldus7519@gmail.com>
2023-08-02 13:00:46 +0900

    [feat] 메인페이지 최근 본 상품 기능 구현

--- client/js/main.js
+++ client/js/main.js
@@ -1,58 +1,88 @@
 import { swiper } from './swiper.js';
 import { getNode, getNodes, insertFirst, css,insertLast } from '../lib/index.js'
 
-const listItem = getNode('.goods__item');
-const listBox = getNode('.goods__list')
-const sideBar = getNode('.side__ul');
-const buttonTop = getNode('.button__top');
-const buttonBottom = getNode('.button__bottom')
-const sideItem = getNode('.side__item')
-const sideContainer = getNode('.side__container');
 
+// DOM 페이지 로드 시 Swiper 및 로컬 스토리지에서 항목 로드를 처리합니다.
+document.addEventListener("DOMContentLoaded", () => {
+  loadItemsFromStorage();
+  initializeSwiper();
+});
 
-const goodsLists = getNodes('.goods__list');
-
-function insertItems(selectedGoods){
-  const template = /* html */ `
-      <li class="swiper-slide swiper3__item w-[78.5px] pb-2">
-        <a href="#">
-          <img 
-            src="${selectedGoods.image}" 
-            alt="${selectedGoods.name}"
-          />
-        </a>
-      </li>
-    `
-
-    if(sideBar.childElementCount == 0){
-      sideContainer.style.height = '180px';
-      
-    }else if(sideBar.childElementCount == 1){
-      sideContainer.style.height = '275px';
-    }else{
-      sideContainer.style.height = '350px'
-    }
-
-    insertFirst(sideBar, template)
+// 로컬 스토리지에서 항목을 불러와 Swiper에 삽입하는 함수
+function loadItemsFromStorage() {
+  const items = getSelectedItemsFromLocalStorage();
+  items.forEach((item) => {
+    insertItems(item);
+  });
 }
 
+// Swiper 초기화 함수
+function initializeSwiper() {
+  new Swiper(".swiper3", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    mousewheel: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+// 로컬 스토리지에서 선택된 항목 가져오기
+function getSelectedItemsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("selectedItems")) || [];
+}
+
+function insertItems(item) {
+  const slideContainer = document.querySelector(".side__ul");
+  const slide = document.createElement("div");
+  slide.classList.add("swiper-slide");
+  slide.innerHTML = `
+    <li class>
+      <img src="${item.image}" alt="${item.name}" />
+    </li>
+  `;
+  insertFirst(slideContainer, slide.outerHTML);
+}
+
+// 중복된 아이템 여부 확인
+function isItemDuplicated(selectedGoods) {
+  const selectedItems = getSelectedItemsFromLocalStorage();
+
+  for (const item of selectedItems) {
+    if (item.name === selectedGoods.name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// 선택한 상품 저장
+function saveSelectedItem(selectedGoods) {
+  const selectedItems = getSelectedItemsFromLocalStorage();
+  selectedItems.push(selectedGoods);
+  localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
+}
+
+const goodsLists = getNodes(".goods__list");
+
 goodsLists.forEach((list) => {
-  const goodsLink = list.querySelector('.goods__link');
-
-  goodsLink.addEventListener('click', (e) => {
+  const goodsLink = list.querySelector(".goods__link");
+  goodsLink.addEventListener("click", (e) => {
     e.preventDefault();
-
     const link = e.currentTarget;
 
     const name = link
-      .querySelector('.goods__name')
-      .textContent.replace(/\n\s+/g, '');
+      .querySelector(".goods__name")
+      .textContent.replace(/\n\s+/g, "");
 
     const price = link
-      .querySelector('.goods__regPrice')
-      .textContent.replace(/\n\s+/g, '');
+      .querySelector(".goods__regPrice")
+      .textContent.replace(/\n\s+/g, "");
 
-    const image = link.querySelector('img').getAttribute('src');
+    const image = link.querySelector("img").getAttribute("src");
 
     const selectedGoods = {
       name,
@@ -60,141 +90,9 @@ goodsLists.forEach((list) => {
       image,
     };
 
-    insertItems(selectedGoods)
+    if (!isItemDuplicated(selectedGoods)) {
+      insertItems(selectedGoods);
+      saveSelectedItem(selectedGoods);
+    }
   });
 });
-
-
-// goodsLists.forEach((list) => {
-//   const goodsLink = list.querySelector('.goods__link');
-
-//   goodsLink.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     const link = e.currentTarget;
-
-//     const name = link
-//       .querySelector('.goods__name')
-//       .textContent.replace(/\n\s+/g, '');
-
-//     const price = link
-//       .querySelector('.goods__regPrice')
-//       .textContent.replace(/\n\s+/g, '');
-
-//     const image = link.querySelector('img').getAttribute('src');
-
-//     const selectedGoods = {
-//       name,
-//       price,
-//       image,
-//     };
-
-//      // 상품이 이미 저장된 상품 목록에 있는지 확인
-//     let recentGoods = JSON.parse(localStorage.getItem('recentGoods')) || [];
-//     const duplicate = recentGoods.some(
-//       (goods) => goods.name === selectedGoods.name && goods.price === selectedGoods.price && goods.image === selectedGoods.image
-//     );
-
-//     // 중복되지 않은 경우에만 상품 데이터를 추가하고 로컬 스토리지에 저장
-//     if (!duplicate) {
-//       const template =
-//       /* html */
-//       `
-//       <li class="recentItem__field ">
-//                 <a href="#">
-//                   <img
-//                     src="${selectedGoods.image}"
-//                     alt="${selectedGoods.name}"
-//                     class="li_image h-[3.21285rem] w-10"
-//                   />
-//                 </a>
-//               </li>
-//       `;
-      
-//       insertLast(sideBar, template);
-//       insertItems(selectedGoods);
-//       recentGoods.push(selectedGoods);
-//       localStorage.setItem('recentGoods', JSON.stringify(recentGoods));
-//     }
-//   });
-// });
-
-// const goodsLists = document.querySelectorAll('.goods__list');
-// const sideBar = document.querySelector('.side__ul');
-
-
-// let recentGoods = JSON.parse(localStorage.getItem('recentGoods')) || [];
-
-
-// recentGoods.forEach((goods) => {
-//   const listItem = 
-//   /*html*/
-//   `
-//   <li class="recentItem__field">
-//     <a href="#">
-//       <img
-//         src="${goods.image}"
-//         alt="${goods.name}"
-//         class="li_image h-[3.21285rem] w-10"
-//       />
-//     </a>
-//   </li>
-//   `;
-//   insertLast(sideBar, listItem);
-// });
-
-// goodsLists.forEach((list) => {
-//   const goodsLink = list.querySelector('.goods__link');
-
-//   goodsLink.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     const link = e.currentTarget;
-
-//     const name = link
-//       .querySelector('.goods__name')
-//       .textContent.replace(/\n\s+/g, '');
-
-//     const price = link
-//       .querySelector('.goods__regPrice')
-//       .textContent.replace(/\n\s+/g, '');
-
-//     const image = link.querySelector('img').getAttribute('src');
-
-//     const selectedGoods = {
-//       name,
-//       price,
-//       image,
-//     };
-    
-    
-//     const duplicate = recentGoods.some(
-//       (goods) =>
-//         goods.name === selectedGoods.name &&
-//         goods.price === selectedGoods.price &&
-//         goods.image === selectedGoods.image
-//     );
-//     if (!duplicate) {
-//       const template = 
-//       /*html*/
-//       `
-//       <li class="recentItem__field">
-//                 <a href="#">
-//                   <img
-//                     src="${selectedGoods.image}"
-//                     alt="${selectedGoods.name}"
-//                     class="li_image h-[3.21285rem] w-10"
-//                   />
-//                 </a>
-//               </li>
-//       `;
-//       insertLasts(sideBar, template);
-//       recentGoods.push(selectedGoods);
-//       localStorage.setItem('recentGoods', JSON.stringify(recentGoods));
-//     }
-//   });
-// });
-
-// function insertLasts(parent, template) {
-//   parent.insertAdjacentHTML('beforeend', template);
-// }
